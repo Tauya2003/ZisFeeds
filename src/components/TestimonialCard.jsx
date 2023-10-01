9e816063c3ac44968ea6732fc74107b1d893e2ec
Tauya <ayuat2003@gmail.com>
2023-10-01 07:28:44 +0200

    Updated the home page

---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,56 @@
+import { Avatar, Box, Stack, Typography } from "@mui/material";
+import image from "../assets/images/placeholderAvatar.jpg";
+
+const TestimonialCard = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "12px",
+        px: "18px",
+        minWidth: 260,
+        height: 131,
+        borderRadius: "19px",
+        bgcolor: "#67b36b",
+      }}
+    >
+      <Avatar
+        src={image}
+        alt="Takudzwa Hyut"
+        sx={{ width: 67, height: 67, border: "solid 7px #7fd67b" }}
+      />
+
+      <Stack gap={1}>
+        <Typography
+          sx={{
+            color: "#fff",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 13,
+            fontWeight: 700,
+          }}
+        >
+          Takudzwa Hyut
+        </Typography>
+
+        <Typography
+          sx={{
+            color: "#fff",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 6,
+            fontStyle: "italic",
+            textAlign: "justify",
+          }}
+        >
+          "Since switching to their feed products, we have seen a remarkable
+          improvement in the health and performance of our livestock. Our
+          animals are thriving, producing better yields, and showing increased
+          resistance to diseases. We couldn't be happier with the results!"
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default TestimonialCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import broilerStart from "../assets/images/BRANCH LAYOUT BROILER.png";
+import TestimonialCard from "../components/TestimonialCard";
 
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+    <Stack alignItems={"center"} pb={5} width={"100vw"}>
+      <Box
+        sx={{
+          width: "100%",
+          // height: 449,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src={broilerStart}
+          alt="zisfeeds broiler start"
+          style={{ width: "100%" }}
+        />
+      </Box>
 
-export default Home
+      <Box
+        sx={{
+          bgcolor: "#66bb6a",
+          mt: "28px",
+          px: "20px",
+          gap: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "35px",
+          width: { xs: 332 },
+          height: { xs: 221 },
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#fff",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 20,
+            fontWeight: 700,
+          }}
+        >
+          OUR VISION
+        </Typography>
+
+        <Typography
+          sx={{
+            color: "#e8f5e9",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 9,
+            fontWeight: 500,
+            textAlign: "center",
+          }}
+        >
+          Welcome to our world of agricultural feed production, where quality
+          and sustainability meet nourishment. As a leading provider in the
+          industry, we are committed to delivering exceptional feed products
+          that optimize animal health, performance, and overall productivity.
+          With a focus on innovation, rigorous quality control, and a deep
+          understanding of animal nutrition, we are your trusted partner in
+          cultivating healthy livestock and maximizing your farming operations'
+          success. Experience the difference of our premium feed solutions and
+          unlock the full potential of your agricultural endeavors.
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          gap: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            mt: "42px",
+            color: "#4c554e",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 13,
+            fontWeight: 700,
+            textTransform: "uppercase",
+          }}
+        >
+          OUR BRANDS
+        </Typography>
+
+        <Typography
+          sx={{
+            color: "#4c554e",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 8,
+            fontStyle: "italic",
+            textAlign: "center",
+            mx: 5,
+            pb: "30px",
+          }}
+        >
+          Leveraging state-of-the-art technology, we produce nutritionally
+          balanced and high-quality feed that meets the dietary needs of
+          livestock, ensuring optimal growth and health.Tailored to your
+          livestock's specific needs, our team can formulate custom blends that
+          cater to unique dietary requirements, maximizing productivity and
+          overall animal health.
+        </Typography>
+      </Box>
+
+      <Box sx={{ width: "100%", height: 321, bgcolor: "#66bb6a" }}></Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "25px",
+        }}
+      >
+        <Typography
+          sx={{
+            borderTop: "solid 4px #4c554e",
+            color: "#4c554e",
+            fontFamily: "Poppins, sans-sarif",
+            fontSize: 13,
+            fontWeight: 700,
+            textTransform: "uppercase",
+            pt: "7px",
+            mt: "30px",
+          }}
+        >
+          TESTIMONIALS
+        </Typography>
+
+        <Stack
+          direction={"row"}
+          gap={"20px"}
+          pl={"20px"}
+          pb={"50px"}
+          sx={{ width: "100%", overflowX: "auto" }}
+        >
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+        </Stack>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Home;
